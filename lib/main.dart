cbb161a1234b6b6629d41cb70d2eed88b51be5e1
anubhav-18 <anubhavbindal18@gmail.com>
2023-09-14 23:48:01 +0530

    PhoneAuth

--- lib/main.dart
+++ lib/main.dart
@@ -19,13 +19,11 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
       // .then((value) => Get.put(AuthenticationRepository()));
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
-  // final controller = Get.put(OTPcontroller());
-
+  const MyApp({super.key});
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
